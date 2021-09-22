d42de4b0d2668f4fd1b5dd11ec8f07b08e30df29
Sophie <sophiejoliver19+1@gmail.com>
2021-09-22 20:18:31 +0000

    refactor using es6 shorthand syntax

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
