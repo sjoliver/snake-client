a53d09fd8fd858a6e78d04a2be594a6342df43f9
Sophie <sophiejoliver19+1@gmail.com>
2021-09-22 21:09:03 +0000

    adding a process.exit when user inputs ctrl+c

--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 const net = require("net");
+const { stdin, stdout } = require("process");
 const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
