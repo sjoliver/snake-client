2408a9b763aadd83ea9d3a083c87da044b0e95d7
Sophie <sophiejoliver19+1@gmail.com>
2021-09-23 01:07:06 +0000

    refactoring to move IP and PORT into a constants file

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
